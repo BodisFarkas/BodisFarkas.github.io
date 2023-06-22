621d3b3ccd4ef78f9ed28b930884a01b92c99e9a
Bódis Farkas <67932509+BodisFarkas@users.noreply.github.com>
2023-06-22 17:53:13 +0200

    v4 404page, working page

--- ERROR/wk.js
+++ ERROR/wk.js
@@ -1,7 +1,7 @@
 //random hatter
 window.onload = choosePic;
 
-var kepk = new Array('./img/bg/1.jpg','./img/bg/2.jpg','./img/bg/3.png','./img/bg/4.jpg','./img/bg/5.jpg','./img/bg/6.jpg','./img/bg/7.jpg','./img/bg/8.jpg','./img/bg/9.jpg');
+var kepk = new Array('../img/bg/1.jpg','../img/bg/2.jpg','../img/bg/3.png','../img/bg/4.jpg','../img/bg/5.jpg','../img/bg/6.jpg','../img/bg/7.jpg','../img/bg/8.jpg','../img/bg/9.jpg');
 
 function choosePic() {
 	//random szám
